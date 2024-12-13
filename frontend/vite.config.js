cafd1c727000b5846526dd02d329c48e5571b4ad
YKuts <yuliiakuts@gmail.com>
2024-12-13 14:11:44 +0100

    Gange config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: process.env.PORT || 3000
+  },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: true
+  }
 })
